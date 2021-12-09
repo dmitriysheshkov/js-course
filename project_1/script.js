0e47fa0ddb9a12eb6bdcb2940edd9d51b157c0bf
Dmitriy Sheshkov <hello@sheshkov.ru>
2021-12-09 20:09:14 +0300

    продолжил работу над приложением

--- project_1/script.js
+++ project_1/script.js
@@ -8,11 +8,27 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    let filmName = prompt('Один из последних просмотренных фильмов?');
-    let filmRating = prompt('На сколько оцените его?');
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы - классический зритель!');
+} else if (personalMovieDB.count >= 30) {
+    alert('Да Вы киноман!');
+} else {
+    alert('Произошла ошибка!');
+}
 
-    personalMovieDB.movies[filmName] = filmRating;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?'),
+          b = prompt('На сколько оцените его?');
+
+    if (a != '' && b != '' && a != null && b != null && a.length < 50 && b.length < 50) {
+        console.log('done');
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log('error');
+        i--;
+    }
 }
 
 // personalMovieDB.count = numberOfFilms;
