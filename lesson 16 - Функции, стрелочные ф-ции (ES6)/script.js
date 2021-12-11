70d23664fe1ca2c33b6a9d69ad11360d9a4e5fb2
Dmitriy Sheshkov <hello@sheshkov.ru>
2021-12-11 19:37:08 +0300

    продолжает изучение функций

--- lesson 16 - Функции, стрелочные ф-ции (ES6)/script.js
+++ lesson 16 - Функции, стрелочные ф-ции (ES6)/script.js
@@ -1,8 +1,22 @@
 // объявление функций
 
-// 
+// объявление функций function declaration (function definition, function statement)
 function showFirstMessage (text) { // в скобках ПАРАМЕТРЫ, а передавать будем аргументы
     console.log(text);
 }
 
 showFirstMessage('Hello World!');
+
+// как ведут себя переменные внутри функций?
+let num = 20;
+
+function showFirstMessage (text) { // в скобках ПАРАМЕТРЫ, а передавать будем аргументы
+    console.log(text);
+    let num = 10;
+    return 77;
+}
+
+// showFirstMessage('Hello!');
+
+// console.log(num);
+console.log(showFirstMessage('Hello!'));
